4d6c9f10dba7e4118f8f28dcc0436199a96965b9
Bhavishya-a <85496477+Bhavishya-a@users.noreply.github.com>
2022-05-25 19:47:56 +0530

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -36,7 +36,7 @@ message = message_data['message'];
 like = message_data['like'];
 name_with_tag = "<h4>" + name +"<img class='user_tick' src = 'tick.png'></h4>";
 message_with_tag = "<h4 class='message_h4'>" + message + "</h4>";
-like_button = "<button class='btn btn-info' id="+ firebase_message_id + "value = " + like +"onclick = 'updateLike(this.id)'>";
+like_button = "<button class='btn btn-info' id="+ firebase_message_id + " value = " + like +" onclick = 'updateLike(this.id)'>";
 span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like:" + like + "</span> </button> <hr>";
 row = name_with_tag + message_with_tag + like_button +span_with_tag;
 document.getElementById("output").innerHTML += row;  
@@ -46,8 +46,8 @@ getData();
 function updateLike(message_id) {
 console.log("click on the like button - " + message_id);
 button_id  = message_id;
-like = document.getElementById(button_id).value;
-update_Likes = Number(like)  + 1;
+likes = document.getElementById(button_id).value;
+update_Likes = Number(likes)  + 1;
 console.log(update_Likes);
 firebase.database().ref(room_name).child(message_id).update({
       like:update_Likes
